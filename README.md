3c2cb45ce95ff591b9872e160648758ecd8ff835
isiah_lloyd <robotdude101@gmail.com>
2015-01-02 17:33:32 -0500

    Fixed setup.py so the project could register on PyPi

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-frcpy
+frcdatapy: A Python API wrapper for the official FRC (FIRST Robotics Competition) API  
 =====
 
-A Python API wrapper for the official FRC (FIRST Robotics Competition) API  
+Simplifies calls to the FRC API by not having to mess with the requests and just get the data you want. Automatically converts the json responses to python dictionaries.
 
-TODO: Documentation
+###Installation:
+------------------
+#####Using Pip:
+
+`pip install frcdatapy`
+
+#####From source:
+
+```
+git clone https://github.com/isiah-lloyd/frcdatapy.git
+cd frcdatapy
+sudo python setup.py install
+```
+
+###Quickstart:
+
+
--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup
 
-setup(name='frcpy',
+with open('README.md') as file_readme:
+      readme = file_readme.read()
+setup(name='frcdatapy',
       version='0.1',
       description='API wrapper for the offical FRC (FIRST Robotics Competition) API',
-      url='N/A',
+      long_description=readme ,
+      url='https://github.com/isiah-lloyd/frcdatapy',
       author='Isiah Lloyd',
       author_email='lloyd@isiah.me',
       license='MIT',
-      packages=['frcpy'],
+      classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English', 
+            'Topic :: Software Development'
+      ],
+      packages=['frcdatapy'],
 	  install_requires=[
           'requests',
       ],
-      zip_safe=False)
+      zip_safe=True)
